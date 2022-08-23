d723614061b89366196ad923984b9d235b16512d
Miguelps11 <miguelpsessa@gmail.com>
2022-08-23 14:02:41 +0200

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.scss'
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <div className="navContainer">
+            <div className="logoContainer">
+                <h1>logo</h1>
+            </div>
+            <div className="navList">
+            <h1>lsta</h1>
+            </div>
+        </div>
+        <div className="navBtn">
+        <h1>Btn</h1>
+        </div>
+    </nav>
+  )
+}
